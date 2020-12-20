1327e10d4bc8f5bf96e3eb00f5e98480bc671135
chinoportela <matiasportela94@gmail.com>
2020-12-20 16:01:08 -0300

    changed page title

--- Views/header.php
+++ Views/header.php
@@ -7,7 +7,7 @@
   <meta name="description" content="" />
   <meta name="author" content="" />
 
-  <title>Gym & Box - Training Center</title>
+  <title>Log In System</title>
 
   <!-- Bootstrap core CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
